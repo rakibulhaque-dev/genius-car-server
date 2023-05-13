dbc4a839883badf4c4cc28081dc720191062c8cb
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-13 09:26:03 +0600

    Booking data functionalised

--- index.js
+++ index.js
@@ -26,18 +26,47 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services')
-    app.get('/services', async(req, res)=>{
+    const bookingCollection = client.db('carDoctor').collection('bookings')
+
+    app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray(cursor);
       res.send(result);
     })
 
-    app.get('/services/:id', async(req, res)=>{
+    app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
-      const result = await serviceCollection.findOne(query);
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1 }
+      }
+
+      const result = await serviceCollection.findOne(query, options);
       res.send(result);
     })
+
+
+    // bookings
+    app.get('/bookings', async (req, res) => {
+      console.log(req.query.email)
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking)
+      const result = await bookingCollection.insertOne(booking)
+      res.send(result)
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -52,9 +81,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Car Data server is running')
+  res.send('Car Data server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Server is running at ${port}`)
+  console.log(`Server is running at ${port}`)
 })
